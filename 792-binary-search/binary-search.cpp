85e2067e8ee24bf46d66535f25439fd0e0f2b4af
Mogul manoj M <mogulmanoj07@gmail.com>
2025-08-11 18:47:16 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (9.50%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-       for(int i=0;i<nums.size();i++){
-        if(nums[i]==target)
-        return i;
-       }return -1;
+        int low=0,high=nums.size() - 1,mid;
+        while(low<=high){
+            mid = (low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                low = mid + 1;
+            }
+            else if(nums[mid]>target){
+                high = mid - 1;
+            }
+        } return -1;
     }
 };
