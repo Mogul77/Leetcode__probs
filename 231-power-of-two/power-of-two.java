fd0f337e65bbe71ba1d7e9d9e65441e2264aeb8b
Mogul manoj M <mogulmanoj07@gmail.com>
2025-12-14 17:26:48 +0530

    Time: 1 ms (75.77%) | Memory: 42.7 MB (6.05%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,9 +1,13 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if( (n>0) && (n & (n-1)) == 0){
-            return true;
+        if(n<=0)
+        return false;
+        while(n%2==0){
+            n = n/2;
         }
-        else
-            return false;
+        if(n==1)
+        return true;
+        else 
+        return false;
     }
 }
