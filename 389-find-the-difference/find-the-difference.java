b7753181ed4b263dff619a4ef61ba86393a1791c
Mogul manoj M <mogulmanoj07@gmail.com>
2025-12-27 16:34:13 +0530

    Time: 3 ms (40.64%) | Memory: 43.5 MB (7.86%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char S[] = s.toCharArray();
+        char T[] = t.toCharArray();
+        Arrays.sort(S);
+        Arrays.sort(T);
+        for(int i =0;i<S.length;i++){
+            if(S[i]!=T[i])
+            return T[i];
+        }
+        return (T[t.length() -1]);
+    }
+}
