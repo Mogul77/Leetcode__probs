a31b14251b3dd3c3bdc3e24f0b21f1a465b88482
Mogul manoj M <mogulmanoj07@gmail.com>
2026-01-13 16:52:13 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (77.32%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -1,16 +1,18 @@
 class Solution {
     public int countNumbersWithUniqueDigits(int n) {
-        if (n == 0) return 1;
-
-        int total = 10;          
-        int product = 9;         
-        int available = 9;       
-        for (int digits = 2; digits <= n && digits <= 10; digits++) {
-            product *= available; 
-            total += product;       
-            available--;            
+        if(n==0)
+        return 1;
+        if(n==1)
+        return 10;
+        int res = 10;
+        int unique = 9;
+        int available =9;
+        for(int i=2;i<=n && available>0;i++){
+            unique = unique*available;
+            res = unique+res;
+            available--;
         }
+        return res;
 
-        return total;
     }
 }
