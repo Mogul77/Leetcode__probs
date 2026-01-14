bb94da85b786812ff2dcd2d708c53d8d5d012e01
Mogul manoj M <mogulmanoj07@gmail.com>
2026-01-14 16:18:06 +0530

    Time: 1 ms (17.00%) | Memory: 42.6 MB (47.37%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
@@ -2,6 +2,17 @@ class Solution {
     public String toHex(int num) {
        if(num==0)
        return "0";
-       return Integer.toHexString(num);
+       long n = num;
+       if(n<0){
+        n= n+(1l<<32);
+       }
+       String hex = "0123456789abcdef";
+       StringBuilder sb = new StringBuilder();
+       while(n>0){
+        int rem = (int) (n%16);
+        sb.append(hex.charAt(rem));
+        n=n/16;
+       }
+       return sb.reverse().toString();
     }
 }
