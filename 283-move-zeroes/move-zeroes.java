6d31190946a5bacc6d98d8f785775b0aa8ad1411
Mogul manoj M <mogulmanoj07@gmail.com>
2026-04-30 16:07:58 +0530

    Time: 1 ms (99.85%) | Memory: 47.6 MB (87.57%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,14 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0;
         int n = nums.length;
-        for(int i = 0;i<n;i++){
+        int ind = 0;
+        for(int i =0;i<n;i++){
             if(nums[i]!=0){
-                int temp = nums[i];
-                nums[i] = nums[left];
-                nums[left]=temp;
-                left++;
+                nums[ind]=nums[i];
+                ind++;
             }
+        }
+        while(ind<n){
+            nums[ind]=0;
+            ind++;
         }
     }
 }
